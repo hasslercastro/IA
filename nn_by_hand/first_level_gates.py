329cd98e478768a00f6a479ffd5744b8e1a427b3
hasslercastro <hasslercastro@gmail.com>
2019-03-25 09:38:31 -0500

    update rule fixed xd

--- nn_by_hand/first_level_gates.py
+++ nn_by_hand/first_level_gates.py
@@ -10,25 +10,25 @@ def forward(X, w, b):
     z = sigmoid((w @ X) + b)
     return z
 
-def cost(y , y_hat):
-    return y - y_hat
+def cost(y_hat, y):
+    return y_hat - y
 
 def back_and_grad(X , y , w , b, learning_rate , num_ep):
 
     for i in range(num_ep):
         z = forward(X,w,b)
-        error = cost( y , z)
+        error = cost( z , y)
         der_w = (error * sigmoid_prime(z)) @  X.T
         der_b = error @ sigmoid_prime(z).T
-        w = w + learning_rate * der_w
-        b = b + learning_rate * der_b
-        print(z)
+        w = w - learning_rate * der_w
+        b = b - learning_rate * der_b
+        print(w,b)
 
     
 
 w = np.random.rand(1,2)
 b = np.random.rand(1)
 X = np.array([[1,1,0,0], [1,0,1,0]])
-y = np.array([1,1,1,0])
-learning_rate = 0.001
+y = np.array([1,0,0,0])
+learning_rate = 0.01
 back_and_grad(X , y , w, b , learning_rate, 20000)
